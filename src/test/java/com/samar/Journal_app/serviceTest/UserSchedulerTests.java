ee058e47198c4623466c80fe07476177dd61e0dc
samar Krishna <amitkumar231677@gmail.com>
2025-12-20 19:37:42 +0530

    removing tests

--- src/test/java/com/samar/Journal_app/serviceTest/UserSchedulerTests.java
+++ src/test/java/com/samar/Journal_app/serviceTest/UserSchedulerTests.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 public class UserSchedulerTests {
     @Autowired
     private UserScheduler userScheduler;
 
-    @Test
+
     public void testFetchUserAndSendMail(){
         userScheduler.fetchUsersAndSendMail();
     }
